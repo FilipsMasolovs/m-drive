53d111379f96278dd1ced451e898b338f08f573a
Filips Masolovs <fmasolovs@onairent.live>
2025-02-16 11:59:45 +0200

    feat: add drizzle-singlestore connect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,14 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["m-drive_*"],
+  dbCredentials: {
+    host: "svc-7ba08cfa-3e59-458b-b9ae-ae06a80aaa95-dml.aws-virginia-8.svc.singlestore.com",
+    user: "admin",
+    password: "WXDf02Wq9I6LhqPVYq5UvCqMW6zW7J3i",
+    port: 3306,
+    database: "M_DRIVE_DB",
+    ssl: {},
+},
 } satisfies Config;
