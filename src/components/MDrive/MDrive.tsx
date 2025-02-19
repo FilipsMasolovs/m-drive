a6aeed0c4976bc468bc49eb3fa7b8cf867e553a0
Filips Masolovs <fmasolovs@onairent.live>
2025-02-19 19:19:50 +0200

    feat: add proper app logo

--- src/components/MDrive/MDrive.tsx
+++ src/components/MDrive/MDrive.tsx
@@ -105,7 +105,16 @@ export default function MDrive({ files, folders, parents, currentFolderId, rootF
   return (
     <div className={styles.pageContainer}>
       <header className={styles.headerContainer}>
-        <GlobalSearch handleItemClick={handleItemClick} />
+        <div className={styles.headerLeftSide}>
+          <a href={`/m/${rootFolderId}`}>
+            <svg width="54" height="54" viewBox="0 0 32 32" fill="none">
+              <rect width="32" height="32" rx="8" fill="#1f2937" />
+              <path d="M7 16L16 7L25 16L16 25L7 16Z" fill="#2C4F7C" stroke="#2C4F7C" strokeWidth="2" strokeLinejoin="round" />
+              <path d="M16 7V25" stroke="#0A1622" strokeWidth="2" strokeLinecap="round" />
+            </svg>
+          </a>
+          <GlobalSearch handleItemClick={handleItemClick} />
+        </div>
         <Actions capacityUsed={capacityUsed} maxCapacity={maxCapacity} />
       </header>
       <main className={styles.listContainer}>
